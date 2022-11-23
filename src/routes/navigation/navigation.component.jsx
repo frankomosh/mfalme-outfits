ede1119f4443e2f81b7361cdc9c9fb4348058054
Frank-197 <frankomosh197@gmail.com>
2022-11-23 12:43:44 +0300

    Improving state management with redux

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,15 @@
 
 import { Fragment, useContext } from "react";
 import { Outlet} from "react-router-dom";
+
+import { useSelector } from "react-redux";
+
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 // import './navigation.styles.scss';
 import { NavigationContainer, NavLinks, NavLink, LogoContainer } from "./navigation.styles";
-import { UserContext } from "../../contexts/user.context";
+// import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
 import { signOutUser  } from "../../utils/firebase/firebase.utils";
 
@@ -14,7 +17,7 @@ import { signOutUser  } from "../../utils/firebase/firebase.utils";
 
 
 const Navigation=()=>{
-    const {currentUser}=useContext(UserContext);
+  
     const {isCartOpen}=useContext(CartContext)
     
     // console.log(currentUser);
