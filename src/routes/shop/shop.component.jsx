2e4981ccbfba881db56518f409b246dcde81b468
Frank-197 <frankomosh197@gmail.com>
2022-11-27 11:31:01 +0300

    modifying business selectors

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,17 +4,15 @@ import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategoriesMap } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.action';
 
 const Shop = ()=>{
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
     
     useEffect(()=>{
         const getCateriesMap=async()=>{
-            const categoryMap= await getCategoriesAndDocuments()
-            // console.log(categoryMap)
-
-            dispatch(setCategoriesMap(categoryMap));
+            const categoriesArray= await getCategoriesAndDocuments('categories')
+            dispatch(setCategories(categoriesArray));
         }
         getCateriesMap();
         
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,118 +1,3 @@
-// import {initializeApp} from 'firebase/app';
-// import {
-//   getAuth, 
-//   signInWithPopup,
-//   signInWithRedirect, 
-//   GoogleAuthProvider, 
-//   createUserWithEmailAndPassword, 
-//   signInWithEmailAndPassword,
-//   signOut,
-//   onAuthStateChanged
-// } from 'firebase/auth';
-// import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
-
-
-
-// const firebaseConfig = {
-    
-//     apiKey: "AIzaSyAPT7_IXeqMaoIgtKnqlfJYcNggAZmnsXQ",
-//     authDomain: "mfalme-outfits.firebaseapp.com",
-//     projectId: "mfalme-outfits",
-//     storageBucket: "mfalme-outfits.appspot.com",
-//     messagingSenderId: "333663862051",
-//     appId: "1:333663862051:web:c11ed00214e19ef7146096",
-//     // measurementId: "G-WKGH9G53N2"
-
-
-//   };
-  
-
-//    const firebaseApp = initializeApp(firebaseConfig);
-
-//   const googleProvider = new GoogleAuthProvider();
-//   googleProvider.setCustomParameters({
-//     prompt: "select_account",
-//   });
-//   export const auth = getAuth();
-//   export const signInWithGooglePopup=()=> signInWithPopup(auth, googleProvider);
-
-  
-//   export const db = getFirestore();
-
-//   export const addCollectionAndDocuments= async (collectionKey, objectsToAdd, field)=>{
-//     const collectionRef=collection(db, collectionKey)
-//     const batch =writeBatch(db);
-
-//     objectsToAdd.forEach((object)=>{
-//       const docRef=doc(collectionRef, object.title.toLowerCase());
-//       batch.set(docRef, object);
-//     });
-//     await batch.commit();
-//     console.log('done');
-
-//   };  
-//   export const getCategoriesAndDocuments = async()=>{
-//     const collectionRef=collection(db, 'categories');
-//     const q=query(collectionRef);
-//     const querySnapshot= await getDocs(q);
-//     const categoryMap=querySnapshot.docs.reduce((acc, docSnapshot)=>{
-//       const{title, items}=docSnapshot.data();
-//       acc[title.toLowerCase()]=items;
-//       return acc;
-
-//     }, {});
-
-//     return categoryMap;
-
-//   }
-
-//   export const createUserDocumentFromAuth = async (
-//     userAuth, 
-//     additionalInformation={}
-//     ) => {
-//     if(!userAuth) return;
-//      const userDocRef=doc(db, 'users', userAuth.uid);
-
-//      console.log(userDocRef)
-
-//      const userSnapshot = await getDoc(userDocRef)
-
-//      console.log(userSnapshot.exists());
-
-//      if(! userSnapshot.exists()){
-//         const {displayName, email}=userAuth;
-//         const createdAt = new Date();
-//         try{
-//             await setDoc(userDocRef, {
-//                 displayName, 
-//                 email, 
-//                 createdAt,
-//                 ...additionalInformation,
-//             });
-//         }catch(error){
-//             console.log('error creating the user', error.message);
-//         }
-//      }
-
-//      return userDocRef
-
-//   }
-//   export const createAuthUserWithEmailAndPassword = async (email, password)=>{
-//     if (!email || !password) return;
-
-//    return await createUserWithEmailAndPassword(auth, email, password)
-//   }
-
-//   export const signInAuthUserWithEmailAndPassword = async (email, password)=>{
-//     if (!email || !password) return;
-
-//    return await signInWithEmailAndPassword(auth, email, password)
-//   }
-//   export const signOutUser = async () => await signOut(auth);
-
-//   export const onAuthStateChangedListener= (callback)=> onAuthStateChanged(auth, callback)
-
-
 import { initializeApp } from 'firebase/app';
 import {
   getAuth,
@@ -136,13 +21,6 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  // authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  // projectId: 'crwn-clothing-db-98d4d',
-  // storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  // messagingSenderId: '626766232035',
-  // appId: '1:626766232035:web:506621582dab103a4d08d6',
-
   apiKey: "AIzaSyAPT7_IXeqMaoIgtKnqlfJYcNggAZmnsXQ",
   authDomain: "mfalme-outfits.firebaseapp.com",
   projectId: "mfalme-outfits",
@@ -191,13 +69,16 @@ export const getCategoriesAndDocuments = async () => {
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
-  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot.data();
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+  return querySnapshot.docs.map(docSnapshot=>docSnapshot.data());
+ 
+  
+  // .reduce((acc, docSnapshot) => {
+  //   const { title, items } = docSnapshot.data();
+  //   acc[title.toLowerCase()] = items;
+  //   return acc;
+  // }, {});
 
-  return categoryMap;
+  // return categoryMap;
 };
 
 export const createUserDocumentFromAuth = async (
